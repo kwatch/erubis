62ebde5610bb41ff0335ffd3d11699a532726975
makoto kuwata <kwa@kuwata-lab.com>
2008-01-29 08:40:39 +0000

    - [enhance] add 'helpers/rails_form_helper.rb' - [change] helpers/rails_helper.rb: rename _expr() to _p() - [enhance] helpers/rails_helper.rb: add _P() - [enhance] helpers/rails_helper.rb: support Rails 2.0 - [update] users-guide.txt - [change] rename 'CHANGES' to 'CHANGES.txt'

--- lib/erubis/helpers/rails_helper.rb
+++ lib/erubis/helpers/rails_helper.rb
@@ -34,15 +34,11 @@ module Erubis
     ##      #Erubis::Helpers::RailsHelper.show_src = false       # set true for debugging
     ##      #Erubis::Helpers::RailsHelper.preprocessing = true   # set true to enable preprocessing
     ##
-    ## 2. (optional) apply the patch for 'action_view/base.rb'
+    ## 2. restart web server.
     ##
-    ##      $ cd /usr/local/lib/ruby/gems/1.8/gems/actionpack-1.13.3/lib/action_view/
-    ##      $ sudo patch -p1 < /tmp/erubis_2.X.X/contrib/action_view_base_rb.patch
-    ##
-    ## 3. restart web server.
-    ##
-    ## if Erubis::Helper::Rails.show_src is ture, Erubis prints converted Ruby code
-    ## into log file ('log/development.log' or so). This may be useful for debug.
+    ## if Erubis::Helper::Rails.show_src is true, Erubis prints converted Ruby code
+    ## into log file ('log/development.log' or so). if false, it doesn't.
+    ## if nil, Erubis prints converted Ruby code if ENV['RAILS_ENV'] == 'development'.
     ##
     module RailsHelper
 
@@ -70,7 +66,7 @@ module Erubis
       end
 
       #cattr_accessor :show_src
-      @@show_src = false
+      @@show_src = nil
 
       def self.show_src
         @@show_src
@@ -80,7 +76,7 @@ module Erubis
         @@show_src = flag
       end
 
-      ##----- preprocessor: experimental -----
+      ##----- preprocessing --------------------
       @@preprocessing = false
       def self.preprocessing
         @@preprocessing
@@ -126,19 +122,21 @@ unless ActionView::Base.private_instance_methods.include?(method_name) ||
       end
 
 
-if ActionPack::VERSION::MINOR <= 12   ###  Rails 1.1
+if ActionPack::VERSION::MAJOR >= 2   ###  Rails 2.X
 
 
-      # Create source code for given template
+      # Method to create the source code for a given template.
       def create_template_source(extension, template, render_symbol, locals)
         if template_requires_setup?(extension)
           body = case extension.to_sym
-            when :rxml
+            when :rxml, :builder
+              content_type_handler = (controller.respond_to?(:response) ? "controller.response" : "controller")
+              "#{content_type_handler}.content_type ||= Mime::XML\n" +
               "xml = Builder::XmlMarkup.new(:indent => 2)\n" +
-              "@controller.headers['Content-Type'] ||= 'application/xml'\n" +
-              template
+              template +
+              "\nxml.target!\n"
             when :rjs
-              "@controller.headers['Content-Type'] ||= 'text/javascript'\n" +
+              "controller.response.content_type ||= Mime::JS\n" +
               "update_page do |page|\n#{template}\nend"
           end
         else
@@ -152,14 +150,14 @@ if ActionPack::VERSION::MINOR <= 12   ###  Rails 1.1
 
         locals_code = ""
         locals_keys.each do |key|
-          locals_code << "#{key} = local_assigns[:#{key}] if local_assigns.has_key?(:#{key})\n"
+          locals_code << "#{key} = local_assigns[:#{key}]\n"
         end
 
         "def #{render_symbol}(local_assigns)\n#{locals_code}#{body}\nend"
       end
 
 
-else    ###  Rails 1.2 or later
+elsif ActionPack::VERSION::MINOR > 12    ###  Rails 1.2
 
 
       # Create source code for given template
@@ -192,6 +190,39 @@ else    ###  Rails 1.2 or later
       end
 
 
+elsif ActionPack::VERSION::MINOR <= 12   ###  Rails 1.1
+
+
+      # Create source code for given template
+      def create_template_source(extension, template, render_symbol, locals)
+        if template_requires_setup?(extension)
+          body = case extension.to_sym
+            when :rxml
+              "xml = Builder::XmlMarkup.new(:indent => 2)\n" +
+              "@controller.headers['Content-Type'] ||= 'application/xml'\n" +
+              template
+            when :rjs
+              "@controller.headers['Content-Type'] ||= 'text/javascript'\n" +
+              "update_page do |page|\n#{template}\nend"
+          end
+        else
+          #body = ERB.new(template, nil, @@erb_trim_mode).src
+          body = convert_template_into_ruby_code(template)
+        end
+
+        @@template_args[render_symbol] ||= {}
+        locals_keys = @@template_args[render_symbol].keys | locals
+        @@template_args[render_symbol] = locals_keys.inject({}) { |h, k| h[k] = true; h }
+
+        locals_code = ""
+        locals_keys.each do |key|
+          locals_code << "#{key} = local_assigns[:#{key}] if local_assigns.has_key?(:#{key})\n"
+        end
+
+        "def #{render_symbol}(local_assigns)\n#{locals_code}#{body}\nend"
+      end
+
+
 end   ###
 
 
@@ -210,10 +241,12 @@ class ActionView::Base  # :nodoc:
     klass      = Erubis::Helpers::RailsHelper.engine_class
     properties = Erubis::Helpers::RailsHelper.init_properties
     show_src   = Erubis::Helpers::RailsHelper.show_src
-    ##----- preprocessing: experimental -----
+    show_src = ENV['RAILS_ENV'] == 'development' if show_src.nil?
+    ##----- preprocessing -------------------
     if Erubis::Helpers::RailsHelper.preprocessing
       preprocessor = Erubis::Helpers::RailsHelper::PreprocessingEruby.new(template)
-      template = self.instance_eval(preprocessor.src)
+      #template = self.instance_eval(preprocessor.src)
+      template = preprocessor.evaluate(self)
       logger.debug "** Erubis: preprocessed==<<'END'\n#{template}END\n" if show_src
     end
     ##----------------------------------------
@@ -223,12 +256,14 @@ class ActionView::Base  # :nodoc:
     src
   end
 
-  ##----- preprocessing: experimental -----
-  def _expr(arg)
+  ##----- preprocessing -------------------
+  def _p(arg)
     return "<%=#{arg}%>"
   end
-  alias _x _expr
-  alias _? _expr
+  def _P(arg)
+    return "<%=h(#{arg})%>"
+  end
+  alias _? _p
   def _decode(arg)
     arg = arg.to_s
     arg.gsub!(/%3C%25%3D(.*?)%25%3E/) { "<%=#{CGI.unescape($1)}%>" }
